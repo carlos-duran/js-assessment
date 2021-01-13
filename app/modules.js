476b0c52cb6402bbe4992a0bfc5b7b4be38f258d
Carlos Duran <carlosduran.xyz@gmail.com>
2021-01-12 21:31:56 -0500

    Module pattern done

--- app/modules.js
+++ app/modules.js
@@ -1,7 +1,15 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.modulesAnswers = {
-  createModule: function(str1, str2) {
+  createModule: function (str1, str2) {
+    const myModule = {
+      name: str2,
+      greeting: str1,
+      sayIt() {
+        return `${this.greeting}, ${this.name}`;
+      },
+    };
 
-  }
+    return myModule;
+  },
 };
