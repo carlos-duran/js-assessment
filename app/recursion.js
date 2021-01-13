8a425b9722b4b3330cbb4aa3369abcbca1c21df1
Carlos Duran <carlosduran.xyz@gmail.com>
2021-01-12 21:26:56 -0500

    Recursion done

--- app/recursion.js
+++ app/recursion.js
@@ -1,19 +1,30 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.recursionAnswers = {
-  listFiles: function(data, dirName) {
+  listFiles: function (data, dirName) {
+    const filesList = [];
 
+    if (dirName) {
+      const dir = data.files.find((e) => e.dir === dirName);
+      if (!dir) {
+        throw new Error("Directory was not found");
+      }
+      return recursionAnswers.listFiles(dir);
+    }
+
+    for (const element of data.files) {
+      if (typeof element === "string") {
+        filesList.push(element);
+      } else {
+        filesList.push(...recursionAnswers.listFiles(element));
+      }
+    }
+    return filesList;
   },
 
-  permute: function(arr) {
+  permute: function (arr) {},
 
-  },
+  fibonacci: function (n) {},
 
-  fibonacci: function(n) {
-
-  },
-
-  validParentheses: function(n) {
-
-  }
+  validParentheses: function (n) {},
 };
